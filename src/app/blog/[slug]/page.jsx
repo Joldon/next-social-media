efa94213d3d277ff0ebf69d029ef9c9d7f0378f3
joldon <79482239+Joldon@users.noreply.github.com>
2024-03-08 18:03:39 +0100

    single post design and content

--- src/app/blog/[slug]/page.jsx
+++ src/app/blog/[slug]/page.jsx
@@ -1,7 +1,43 @@
-import React from "react";
+import Image from "next/image";
+import styles from "./singlePost.module.css";
 
 const SinglePostPage = () => {
-  return <div>SinglePostPage</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.imgContainer}>
+        <Image
+          className={styles.img}
+          src="https://images.pexels.com/photos/20412064/pexels-photo-20412064/free-photo-of-portofino.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load"
+          alt=""
+          fill
+        />
+      </div>
+      <div className={styles.textContainer}>
+        <h1 className={styles.title}>Title</h1>
+        <div className={styles.detail}>
+          <Image
+            className={styles.avatar}
+            src="https://images.pexels.com/photos/20412064/pexels-photo-20412064/free-photo-of-portofino.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load"
+            alt=""
+            width={50}
+            height={50}
+          />
+          <div className={styles.detailText}>
+            <span className={styles.detailTitle}>Auhtor</span>
+            <span className={styles.detailValue}>Martin Scousa</span>
+          </div>
+          <div className={styles.detailText}>
+            <span className={styles.detailTitle}>published</span>
+            <span className={styles.detailValue}>01.01.2023</span>
+          </div>
+        </div>
+        <div className={styles.content}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus
+          iste inventore distinctio perferendis maiores repudiandae!
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default SinglePostPage;
