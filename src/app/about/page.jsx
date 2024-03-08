7753675b7e3e4a07f4bbb3ca7bf3f9ec9c709657
joldon <79482239+Joldon@users.noreply.github.com>
2024-03-08 18:04:07 +0100

    about page design and content

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -3,13 +3,36 @@ import styles from "./about.module.css";
 
 const AboutPage = () => {
   return (
-    <div>
+    <div className={styles.container}>
+      <div className={styles.textContainer}>
+        <h2 className={styles.subtitle}>About Agency</h2>
+        <h1 className={styles.title}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam,
+          dolorem!
+        </h1>
+        <p className={styles.desc}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati
+          blanditiis labore non voluptatem reiciendis necessitatibus sunt, nisi
+          odio fugiat accusantium maiores harum enim optio distinctio.
+        </p>
+        <div className={styles.boxes}>
+          <div className={styles.box}>
+            <h1>10 K+</h1>
+            <p>Years of expreience</p>
+          </div>
+          <div className={styles.box}>
+            <h1>10 K+</h1>
+            <p>Years of expreience</p>
+          </div>
+          <div className={styles.box}>
+            <h1>10 K+</h1>
+            <p>Years of expreience</p>
+          </div>
+        </div>
+      </div>
+
       <div className={styles.imgContainer}>
-        <Image
-          src="https://images.pexels.com/photos/20509971/pexels-photo-20509971/free-photo-of-a-reflection-from-the-courtyard-of-zinciriye-medrese.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load"
-          alt=""
-          fill
-        />
+        <Image className={styles.img} src="/about.png" alt="About" fill />
       </div>
     </div>
   );
