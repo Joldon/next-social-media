2e466753375b3988070a5a913bd858b2f84426fc
joldon <79482239+Joldon@users.noreply.github.com>
2024-03-08 14:14:47 +0100

    footer page design and content

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,7 +1,14 @@
-import React from "react";
+import styles from "./footer.module.css";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.logo}></div>
+      <div className={styles.text}>
+        Lama creative thoughts agency. copyright All rights reserved
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
