df0adba4451bd10b3b41a7fbc5ab60e7bac8ecf4
joldon <79482239+Joldon@users.noreply.github.com>
2024-03-08 12:47:04 +0100

    home page design and content

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,30 @@
+import styles from "./home.module.css";
+import Image from "next/image";
 export default function Home() {
-  return <div>Homepage</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.textContainer}>
+        <h1 className={styles.title}>Creative Thoughts Agency</h1>
+        <p className={styles.desc}>
+          Lorem30 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem
+          continue to 30 words.
+        </p>
+        <div className={styles.buttons}>
+          <button className={styles.button}>Learn More</button>
+          <button className={styles.button}>Contact</button>
+        </div>
+        <div className={styles.brands}>
+          <Image
+            src="/brands.png"
+            alt="brands"
+            fill
+            className={styles.brandImg}
+          />
+        </div>
+      </div>
+      <div className={styles.imgContainer}>
+        <Image src="/hero.gif" alt="" fill className={styles.heroImg} />
+      </div>
+    </div>
+  );
 }
