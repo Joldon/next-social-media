db806b75f20e9f96e3983d2d92ecdae4216e1a75
joldon <79482239+Joldon@users.noreply.github.com>
2024-03-08 12:44:29 +0100

    about page content and design

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,7 +1,18 @@
-import React from "react";
+import Image from "next/image";
+import styles from "./about.module.css";
 
 const AboutPage = () => {
-  return <div>AboutPage</div>;
+  return (
+    <div>
+      <div className={styles.imgContainer}>
+        <Image
+          src="https://images.pexels.com/photos/20509971/pexels-photo-20509971/free-photo-of-a-reflection-from-the-courtyard-of-zinciriye-medrese.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load"
+          alt=""
+          fill
+        />
+      </div>
+    </div>
+  );
 };
 
 export default AboutPage;
