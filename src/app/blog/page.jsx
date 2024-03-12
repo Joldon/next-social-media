44ad630263ed6a461107ac285278800dc98892f2
joldon <79482239+Joldon@users.noreply.github.com>
2024-03-12 22:44:26 +0100

    general blog page data fetching

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -1,21 +1,30 @@
+import { getPosts } from "@/lib/data";
 import styles from "./blog.module.css";
 import PostCard from "@/components/postCard/postCard";
 
-const BlogPage = () => {
+// FETCH DATA WITH AN API
+// const getData = async () => {
+//   const res = await fetch("https://jsonplaceholder.typicode.com/posts", {
+//     next: { revalidate: 100 },
+//   });
+//   if (!res.ok) {
+//     throw new Error("Something went wrong");
+//   }
+//   return res.json();
+// };
+const BlogPage = async () => {
+  // FETCH DATA WITH AN API
+  // const posts = await getData();
+
+  // FETCH DATA WITHOUT AN API
+  const posts = await getPosts();
   return (
     <div className={styles.container}>
-      <div className={styles.post}>
-        <PostCard />
-      </div>
-      <div className={styles.post}>
-        <PostCard />
-      </div>
-      <div className={styles.post}>
-        <PostCard />
-      </div>
-      <div className={styles.post}>
-        <PostCard />
-      </div>
+      {posts.map((post) => (
+        <div className={styles.post} key={post.id}>
+          <PostCard post={post} />
+        </div>
+      ))}
     </div>
   );
 };
