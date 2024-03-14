93d28ed8d67502a2bfbb3e9a9f579583858349f9
joldon <79482239+Joldon@users.noreply.github.com>
2024-03-14 23:00:08 +0100

    inserting postCard image

--- src/components/postCard/postCard.jsx
+++ src/components/postCard/postCard.jsx
@@ -6,21 +6,23 @@ const PostCard = ({ post }) => {
   return (
     <div className={styles.container}>
       <div className={styles.top}>
-        <div className={styles.imgContainer}>
-          <Image
-            src="https://images.pexels.com/photos/20451936/pexels-photo-20451936/free-photo-of-nazare.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load"
-            // src="https://images.pexels.com/photos/20412064/pexels-photo-20412064/free-photo-of-portofino.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load"
-            alt=""
-            fill
-            className={styles.img}
-          />
-        </div>
+        {post.img && (
+          <div className={styles.imgContainer}>
+            <Image
+              src={post.img}
+              // src="https://images.pexels.com/photos/20412064/pexels-photo-20412064/free-photo-of-portofino.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load"
+              alt=""
+              fill
+              className={styles.img}
+            />
+          </div>
+        )}
         <span className={styles.date}>01.03.2024</span>
       </div>
       <div className={styles.bottom}>
         <h1 className={styles.title}>{post.title}</h1>
         <p className={styles.desc}>{post.body}</p>
-        <Link className={styles.link} href={`/blog/${post.id}`}>
+        <Link className={styles.link} href={`/blog/${post.slug}`}>
           Read More
         </Link>
       </div>
