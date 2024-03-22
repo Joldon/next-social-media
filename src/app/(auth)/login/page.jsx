f6b5817c46c78f93a50a79d8a2e161069d1e1357
joldon <79482239+Joldon@users.noreply.github.com>
2024-03-22 15:12:48 +0100

    updating login page and login form

--- src/app/(auth)/login/page.jsx
+++ src/app/(auth)/login/page.jsx
@@ -1,16 +1,17 @@
-import { handleGithubLogin, login } from "@/lib/action";
+import LoginForm from "@/components/loginForm/loginForm";
+import { handleGithubLogin } from "@/lib/action";
+import styles from "./login.module.css";
 
-const LoginPage = async () => {
+const LoginPage = () => {
+  // async removed from here because the function does not fetch data
   return (
-    <div>
-      <form action={handleGithubLogin}>
-        <button>Login with Github</button>
-      </form>
-      <form action={login}>
-        <input type="text" placeholder="username" name="username" />
-        <input type="passowrd" placeholder="password" name="password" />
-        <button>Login with Credentials</button>
-      </form>
+    <div className={styles.container}>
+      <div className={styles.wrapper}>
+        <form action={handleGithubLogin}>
+          <button className={styles.github}>Login with Github</button>
+        </form>
+        <LoginForm />
+      </div>
     </div>
   );
 };
