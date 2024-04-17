1435a6f744a79473a1c5e982867e8b0cf625f0b9
joldon <79482239+Joldon@users.noreply.github.com>
2024-04-17 14:12:05 +0200

    fixed typo and added missing action

--- src/components/adminPostForm/adminPostForm.jsx
+++ src/components/adminPostForm/adminPostForm.jsx
@@ -5,9 +5,9 @@ import styles from "./adminPostForm.module.css";
 import { useFormState } from "react-dom";
 
 const AdminPostForm = ({ userId }) => {
-  const [state, forAction] = useFormState(addPost, undefined);
+  const [state, formAction] = useFormState(addPost, undefined);
   return (
-    <form className={styles.container}>
+    <form action={formAction} className={styles.container}>
       <h1>Add new post</h1>
       <input type="hidden" name="userId" value={userId} />
       <input type="text" name="title" placeholder="Title" />
